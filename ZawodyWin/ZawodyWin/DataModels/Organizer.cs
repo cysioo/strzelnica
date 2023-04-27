a9244697fe52b1a142955f1eebd42a91f5434ead
Krzysztof Pawelczyk <krzysztof.pawelczyk@hicron.com>
2023-04-27 12:49:48 +0200

    fixing dbNull error

--- ZawodyWin/ZawodyWin/DataModels/Organizer.cs
+++ ZawodyWin/ZawodyWin/DataModels/Organizer.cs
@@ -1,12 +1,15 @@
-﻿namespace ZawodyWin.DataModels
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZawodyWin.DataModels
 {
+    [Table("Organizer")]
     public class Organizer
     {
         public long Id { get; set; }
         public string Name { get; set; }
-        public string License { get; set; }
-        public string AddressLine1 { get; set; }
-        public string AddressLine2 { get; set; }
-        public string LogoPath { get; set; }
+        public string? License { get; set; }
+        public string? AddressLine1 { get; set; }
+        public string? AddressLine2 { get; set; }
+        public string? LogoPath { get; set; }
     }
 }
