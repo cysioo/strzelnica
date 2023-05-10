3d3b2d9b4feb106b06262cf83c1036756fff76ff
Krzysztof Pawelczyk <krzysztof.pawelczyk@hicron.com>
2023-05-10 11:55:31 +0200

    create scripts not needed since EF migrations work

--- ZawodyWin/ZawodyWin/DB/CreateScripts/Competition.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [Competition] (
-  [Id] INTEGER NOT NULL
-, [Name] TEXT NOT NULL
-, [NumberOfRounds] INTEGER NOT NULL
-, [TournamentId] INTEGER NOT NULL
-, CONSTRAINT [PK_Competition] PRIMARY KEY ([Id])
-);
-
-GO;
