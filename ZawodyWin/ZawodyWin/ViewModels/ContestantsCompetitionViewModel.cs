0eaaac9ee9584beb928e42bea1da577f1450e82f
Krzysztof Pawelczyk <krzysztof.pawelczyk@hicron.com>
2023-05-16 19:46:01 +0200

    Total score updating when points in a round change

--- ZawodyWin/ZawodyWin/ViewModels/ContestantsCompetitionViewModel.cs
+++ ZawodyWin/ZawodyWin/ViewModels/ContestantsCompetitionViewModel.cs
@@ -1,24 +1,72 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Windows;
 using ZawodyWin.DataModels;
 
 namespace ZawodyWin.ViewModels
 {
-    public class ContestantsCompetitionViewModel
+    public class ContestantsCompetitionViewModel : INotifyPropertyChanged
     {
+        private long _totalScore;
+
         public long Id { get; set; }
         public string Name { get; set; }
-        public int TotalScore { get; set; }
+        public long TotalScore
+        {
+            get { return _totalScore; }
+            set
+            {
+                _totalScore = value;
+                OnPropertyChanged(nameof(TotalScore));
+            }
+        }
+
         public ObservableCollection<ScoreViewModel> Scores { get; set; } = new ObservableCollection<ScoreViewModel>();
+
+        public ContestantsCompetitionViewModel()
+        {
+            Scores.CollectionChanged += Scores_CollectionChanged;
+        }
+
+        private void Scores_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            TotalScore = Scores.Sum(x => x.Points);
+            if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                foreach (ScoreViewModel item in e.OldItems)
+                {
+                    //Removed items
+                    item.PropertyChanged -= ScoreViewModelPropertyChanged;
+                }
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                foreach (ScoreViewModel item in e.NewItems)
+                {
+                    //Added items
+                    item.PropertyChanged += ScoreViewModelPropertyChanged;
+                }
+            }
+        }
+
+        public void ScoreViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            TotalScore = Scores.Sum(x => x.Points);
+        }
 
         public void SetFromDbModel(Competition competition)
         {
             Id = competition.Id;
             Name = competition.Name;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
