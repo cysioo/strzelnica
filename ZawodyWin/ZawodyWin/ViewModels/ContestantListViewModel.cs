1d1c2cb47b4ee277c2fde465765134f620165c89
Krzysztof Pawelczyk <krzysztof.pawelczyk@hicron.com>
2023-05-17 11:09:42 +0200

    cleanning up comments and unneeded code

--- ZawodyWin/ZawodyWin/ViewModels/ContestantListViewModel.cs
+++ ZawodyWin/ZawodyWin/ViewModels/ContestantListViewModel.cs
@@ -1,72 +1,29 @@
-﻿using CommunityToolkit.Mvvm.Input;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Windows.Controls;
-using System.Windows;
-using System.Windows.Input;
-using ZawodyWin.DataModels;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using ZawodyWin.DataModels;
 
 namespace ZawodyWin.ViewModels
 {
-    public class ContestantListViewModel : INotifyPropertyChanged
+    public class ContestantListViewModel
     {
         private ObservableCollection<ContestantViewModel> _contestants;
-        private ContestantViewModel _selectedContestant;
-        private bool _isEditing;
 
         public ObservableCollection<ContestantViewModel> Contestants
         {
             get { return _contestants; }
-            set { _contestants = value; OnPropertyChanged(); }
+            set { _contestants = value; }
         }
-
-        //public ContestantViewModel SelectedContestant
-        //{
-        //    get { return _selectedContestant; }
-        //    set { _selectedContestant = value; OnPropertyChanged(); }
-        //}
-
-        //public bool IsEditing
-        //{
-        //    get { return _isEditing; }
-        //    set { _isEditing = value; OnPropertyChanged(); }
-        //}
-
-        //public ICommand EditCommand { get; private set; }
-        //public ICommand SaveCommand { get; private set; }
 
         public ContestantListViewModel()
         {
             Contestants = new ObservableCollection<ContestantViewModel>();
-            
-            //EditCommand = new RelayCommand<ContestantViewModel>(EditContestant);
-            //SaveCommand = new RelayCommand<ContestantViewModel>(SaveContestant);
         }
-
-        //private void EditContestant(ContestantViewModel contestant)
-        //{
-        //    SelectedContestant = contestant;
-        //    IsEditing = true;
-        //}
-
-        //private void SaveContestant(Contestant parameter)
-        //{
-        //    SelectedContestant = null;
-        //    IsEditing = false;
-        //}
 
         public bool DoesContestantExist(Person person)
         {
             return Contestants.Any(x => x.PersonId == person.Id);
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
